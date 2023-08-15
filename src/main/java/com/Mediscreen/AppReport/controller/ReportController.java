6cbab561dcb4b900ee42e790172447d421648ac3
HEITZMANNM <mheitzmann1@hotmail.fr>
2023-08-15 17:03:50 +0200

    final commit

--- src/main/java/com/Mediscreen/AppReport/controller/ReportController.java
+++ src/main/java/com/Mediscreen/AppReport/controller/ReportController.java
@@ -12,17 +12,21 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReportController {
 
     @Autowired
     ReportService reportService;
 
     @PostMapping("/report/generate")
-    public Report generateAReport(@RequestParam(name = "birthdate") @DateTimeFormat(pattern="dd/MM/yyyy") Date birthdate , @RequestParam(name = "gender") String gender , @RequestBody List<PatientAssesmentDTO> list){
+    public Report generateAReport(@RequestParam(name = "birthdate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date birthdate , @RequestParam(name = "gender") String gender , @RequestBody List<PatientAssesmentDTO> list) throws ParseException {
+
 
         List<RiskFactor> listOfRiskFactorFound = reportService.searchForFactorsInAssessments(list);
         RiskLevel riskLevelEvaluated = reportService.evaluateRiskLevel(listOfRiskFactorFound, birthdate, gender);
